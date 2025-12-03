0bf2894e4bf3a718e97862b359ad07550dd02320
Luke Jodice <60670203+ljodiceendicott@users.noreply.github.com>
2025-12-02 23:31:14 -0500

    day 2 completed

--- dec2/dec2.js
+++ dec2/dec2.js
@@ -7,30 +7,32 @@ const data = fs.readFileSync('dec2.txt', 'utf8');
 // Split into lines (if needed)
 const idrange = data.split(',').filter(line => line.trim() !== '');
 
-function hasSameConsecutiveChars(str) {
-    let pattern = /([A-Za-z0-9])\1/g; // Matches any alphanumeric character followed by itself
-    return pattern.test(str);
-  }
 
-  function hasSameConsecutiveChars(str) {
-    let pattern = /([A-Za-z0-9])\1/g; // Matches any alphanumeric character followed by itself
-    return pattern.str;
+  function hasSameConsecutiveCharsout(str) {
+    const valuestr = str.toString();
+    const left = valuestr.slice(0,valuestr.length/2);
+    const right = valuestr.slice(valuestr.length/2);
+    // console.log(left===right)
+
+    return left === right;
   }
 
 let idxtoadd = []
 //loop through all of the combinations
 for(var i = 0; i<idrange.length; i++)
 {
-// console.log('value');
 //Split the start and the end combination up
 let start = parseInt(idrange[i].slice(0,idrange[i].indexOf('-')));
 let end = parseInt(idrange[i].slice(idrange[i].indexOf('-')+1));
 
+
     //this will loop over all values in between start and end
-    for(var j=start; j<end; j++){
-        if(hasSameConsecutiveChars(j)){
-            idxtoadd.push(j);
-        }
+    for(var j=start; j<=end; j++){
+      // console.log(j)
+      if(hasSameConsecutiveCharsout(j.toString())){
+        idxtoadd.push(j);
+      }
+    }
     //     //j is the actual number
     //     let char = j.toString().charAt(0);
     //     console.log(char)
@@ -41,11 +43,11 @@ let end = parseInt(idrange[i].slice(idrange[i].indexOf('-')+1));
     //             idxtoadd.push(j)
     //         }
     //     }
-    }
 }
+// console.log(idxtoadd)
 let sum = 0;
 
-for(var x in idxtoadd){
+for(var x of idxtoadd){
     sum=sum+x;
 }
 console.log(sum)
